aef9fd92511ff8ecb4582e22d4125ce26c50ac0d
Shar1799 <sharmey99@gmail.com>
2021-03-23 02:04:44 +0000

    Semaforo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,57 @@
+//Importar
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	//Creación de un constructor
+	constructor() {
+		super();
+		this.state = {
+			seleccionar: null
+		};
+	}
+
+	//Retorna
+	render() {
+		//Declaración de variables
+		let varRoja = "";
+		let varAmarilla = "";
+		let varVerde = "";
+
+		//Agregar la luz cuando seleccionamos
+		if (this.state.seleccionar == "roja") {
+			varAmarilla = "seleccionar";
+			varVerde = "seleccionar";
+		}
+		if (this.state.seleccionar == "amarilla") {
+			varRoja = "seleccionar";
+			varVerde = "seleccionar";
+		}
+		if (this.state.seleccionar == "verde") {
+			varAmarilla = "seleccionar";
+			varRoja = "seleccionar";
+		}
+
+		return (
+			<div className="container">
+				<div
+					className={"luz roja" + varRoja}
+					onClick={() =>
+						this.setState({ seleccionar: "roja" })
+					}></div>
+				<div
+					className={"luz amarilla" + varAmarilla}
+					onClick={() =>
+						this.setState({ seleccionar: "amarilla" })
+					}></div>
+				<div
+					className={"luz verde" + varVerde}
+					onClick={() =>
+						this.setState({ seleccionar: "verde" })
+					}></div>
+			</div>
+		);
+	}
 }
